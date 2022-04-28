b6fbf1f387a5481dbe3c9a04f241209898b0872b
Ugur Y <uguryuruk@yahoo.com>
2022-04-28 16:42:41 +0300

    Error Handling and Tap added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';  //asenkron işlemler için.
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -22,13 +23,37 @@ private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
 }
 
+/**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+ private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
+
 //asenkron!
 //adds incoming heroes to the messages property of msgs service. Then you use it from there
 /** GET heroes from the server */
 getHeroes(): Observable<Hero[]> {
   return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
 }
-
 getHero(id: number): Observable<Hero> {
   // For now, assume that a hero with the specified `id` always exists.
   // Error handling will be added in the next step of the tutorial.
